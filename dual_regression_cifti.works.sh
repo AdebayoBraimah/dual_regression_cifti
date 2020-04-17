8b3291aee77ac753d1277bb9840f070332463cad
AdebayoBraimah <adebayo.braimah@gmail.com>
2020-04-17 01:20:58 -0400

    Added feature to implement 1-sample t-test; clean-up code later

--- dual_regression_cifti.works.sh
+++ dual_regression_cifti.works.sh
@@ -831,7 +831,7 @@ if [[ ${log_p} = "true" ]]; then
   palm_cmds+="-logp "
 fi
 
-if [[ ${twotail} = "true" ]] && [[ ${one_sample} = "false" ]]; then
+if [[ ${twotail} = "true" ]]; then
   palm_cmds+="-twotail "
 fi
 
@@ -863,23 +863,23 @@ while [ ${j} -lt ${Nics} ] ; do
         -metric CORTEX_RIGHT ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.func.gii ; \
         ${uncompress_cmd}" >> ${LOGDIR}/dr.ciftiD
 
-  # # File and directory handling in the case of one-sample t-tests
-  # if [[ ${one_sample} = "true" ]]; then
-  #   # Internally uncompress gifti files if octave is being used
-  #   if hash octave 2>/dev/null; then
-  #     uncompress_cmd="wb_command -gifti-convert BASE64_BINARY ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii ; \
-  #         wb_command -gifti-convert BASE64_BINARY ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii"
-  #   fi
-  #   # Perform sign-flip of input data for negative contrast
-  #   # PALM's expected behavior should be similar to that of randomise
-  #   # See this link for details: https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/GLM#Single-Group_Average_.28One-Sample_T-Test.29
-  #   echo "mkdir -p ${OUTPUT}/dr_stage3_ic${jj}.palm/vol.neg ; mkdir -p ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L.neg ; mkdir -p ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R.neg ; \
-  #         wb_command -cifti-math '(x * -1)' ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.dscalar.nii -fixnan 0 -var x ${OUTPUT}/dr_stage2_ic${jj}.dscalar.nii ; \
-  #         wb_command -cifti-separate ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.dscalar.nii COLUMN -volume-all ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.nii \
-  #         -metric CORTEX_LEFT ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii \
-  #         -metric CORTEX_RIGHT ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii ; \
-  #         ${uncompress_cmd}" >> ${LOGDIR}/dr.ciftiD
-  # fi
+  # File and directory handling in the case of one-sample t-tests
+  if [[ ${one_sample} = "true" ]]; then
+    # Internally uncompress gifti files if octave is being used
+    if hash octave 2>/dev/null; then
+      uncompress_cmd="wb_command -gifti-convert BASE64_BINARY ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii ; \
+          wb_command -gifti-convert BASE64_BINARY ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii"
+    fi
+    # Perform sign-flip of input data for negative contrast
+    # PALM's expected behavior should be similar to that of randomise
+    # See this link for details: https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/GLM#Single-Group_Average_.28One-Sample_T-Test.29
+    echo "mkdir -p ${OUTPUT}/dr_stage3_ic${jj}.palm/vol.neg ; mkdir -p ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L.neg ; mkdir -p ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R.neg ; \
+          wb_command -cifti-math '(x * -1)' ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.dscalar.nii -fixnan 0 -var x ${OUTPUT}/dr_stage2_ic${jj}.dscalar.nii ; \
+          wb_command -cifti-separate ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.dscalar.nii COLUMN -volume-all ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.nii \
+          -metric CORTEX_LEFT ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii \
+          -metric CORTEX_RIGHT ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii ; \
+          ${uncompress_cmd}" >> ${LOGDIR}/dr.ciftiD
+  fi
 
   j=$(echo "${j} 1 + p" | dc -)
 done
@@ -925,13 +925,13 @@ while [ ${j} -lt ${Nics} ] ; do
   PALM_CMD_surf_L="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.func.gii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L/cort.L -T -tfce2D -s ${template_surf_L} ${left_va} -m ${OUTPUT}/palm.mask/cort.L.mask.func.gii ${palm_cmds}"
   PALM_CMD_surf_R="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.func.gii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R/cort.R -T -tfce2D -s ${template_surf_R} ${right_va} -m ${OUTPUT}/palm.mask/cort.R.mask.func.gii ${palm_cmds}"
 
-  # # In the case of 1-sample t-tests
-  # if [[ ${one_sample} = "true" ]]; then
-  #   # Base PALM commands
-  #   PALM_CMD_vol_neg="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.nii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/vol.neg/vol_neg -T -m ${OUTPUT}/palm.mask/vol.mask.nii ${palm_cmds}"
-  #   PALM_CMD_surf_L_neg="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L.neg/cort.L.neg -T -tfce2D -s ${template_surf_L} ${left_va} -m ${OUTPUT}/palm.mask/cort.L.mask.func.gii ${palm_cmds}"
-  #   PALM_CMD_surf_R_neg="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R.neg/cort.R.neg -T -tfce2D -s ${template_surf_R} ${right_va} -m ${OUTPUT}/palm.mask/cort.R.mask.func.gii ${palm_cmds}"
-  # fi
+  # In the case of 1-sample t-tests
+  if [[ ${one_sample} = "true" ]]; then
+    # Base PALM commands
+    PALM_CMD_vol_neg="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.neg.nii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/vol.neg/vol_neg -T -m ${OUTPUT}/palm.mask/vol.mask.nii ${palm_cmds}"
+    PALM_CMD_surf_L_neg="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.L.neg.func.gii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L.neg/cort.L.neg -T -tfce2D -s ${template_surf_L} ${left_va} -m ${OUTPUT}/palm.mask/cort.L.mask.func.gii ${palm_cmds}"
+    PALM_CMD_surf_R_neg="palm -i ${OUTPUT}/dr_stage3_ic${jj}.palm/dr_stage2_ic${jj}.R.neg.func.gii -o ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R.neg/cort.R.neg -T -tfce2D -s ${template_surf_R} ${right_va} -m ${OUTPUT}/palm.mask/cort.R.mask.func.gii ${palm_cmds}"
+  fi
 
   # Check if PALM jobs need to be submitted
   dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls *tfce*.dscalar.nii) )
@@ -962,25 +962,25 @@ while [ ${j} -lt ${Nics} ] ; do
       bsub -N ${job_cmds} -o ${o_log}_PALM_surfR.log -e ${o_log}_PALM_surfR.err -J ic${jj}_PALM.R -K ${PALM_CMD_surf_R} &
       echo "bsub -N ${job_cmds} -o ${o_log}_PALM_surfR.log -e ${o_log}_PALM_surfR.err -J ic${jj}_PALM.R -K ${PALM_CMD_surf_R}" >> ${LOGDIR}/dr.PALM
     fi
-    # # 1-sample t-tests job submissions
-    # if [[ ${one_sample} = "true" ]]; then
-    #   # Run each job for PALM in parallel - this is faster
-    #   dr_palm_cii=""; dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls vol.neg/*.nii) )
-    #   if [[ ${#dr_palm_cii[@]} -eq 0 ]]; then
-    #     bsub -N ${job_cmds} -o ${o_log}_PALM_vol.neg.log -e ${o_log}_PALM_vol.neg.err -J ic${jj}_PALM.neg.V -K ${PALM_CMD_vol_neg} &
-    #     echo "bsub -N ${job_cmds} -o ${o_log}_PALM_vol.neg.log -e ${o_log}_PALM_vol.neg.err -J ic${jj}_PALM.neg.V -K ${PALM_CMD_vol_neg}" >> ${LOGDIR}/dr.PALM
-    #   fi
-    #   dr_palm_cii=""; dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls cort.L.neg/*.gii) )
-    #   if [[ ${#dr_palm_cii[@]} -eq 0 ]]; then
-    #     bsub -N ${job_cmds} -o ${o_log}_PALM_surfL.neg.log -e ${o_log}_PALM_surfL.neg.err -J ic${jj}_PALM.neg.L -K ${PALM_CMD_surf_L_neg} &
-    #     echo "bsub -N ${job_cmds} -o ${o_log}_PALM_surfL.neg.log -e ${o_log}_PALM_surfL.neg.err -J ic${jj}_PALM.neg.L -K ${PALM_CMD_surf_L_neg}" >> ${LOGDIR}/dr.PALM
-    #   fi
-    #   dr_palm_cii=""; dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls cort.R.neg/*.gii) )
-    #   if [[ ${#dr_palm_cii[@]} -eq 0 ]]; then
-    #     bsub -N ${job_cmds} -o ${o_log}_PALM_surfR.neg.log -e ${o_log}_PALM_surfR.neg.err -J ic${jj}_PALM.neg.R -K ${PALM_CMD_surf_R_neg} &
-    #     echo "bsub -N ${job_cmds} -o ${o_log}_PALM_surfR.neg.log -e ${o_log}_PALM_surfR.neg.err -J ic${jj}_PALM.neg.R -K ${PALM_CMD_surf_R_neg}" >> ${LOGDIR}/dr.PALM
-    #   fi
-    # fi
+    # 1-sample t-tests job submissions
+    if [[ ${one_sample} = "true" ]]; then
+      # Run each job for PALM in parallel - this is faster
+      dr_palm_cii=""; dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls vol.neg/*.nii) )
+      if [[ ${#dr_palm_cii[@]} -eq 0 ]]; then
+        bsub -N ${job_cmds} -o ${o_log}_PALM_vol.neg.log -e ${o_log}_PALM_vol.neg.err -J ic${jj}_PALM.neg.V -K ${PALM_CMD_vol_neg} &
+        echo "bsub -N ${job_cmds} -o ${o_log}_PALM_vol.neg.log -e ${o_log}_PALM_vol.neg.err -J ic${jj}_PALM.neg.V -K ${PALM_CMD_vol_neg}" >> ${LOGDIR}/dr.PALM
+      fi
+      dr_palm_cii=""; dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls cort.L.neg/*.gii) )
+      if [[ ${#dr_palm_cii[@]} -eq 0 ]]; then
+        bsub -N ${job_cmds} -o ${o_log}_PALM_surfL.neg.log -e ${o_log}_PALM_surfL.neg.err -J ic${jj}_PALM.neg.L -K ${PALM_CMD_surf_L_neg} &
+        echo "bsub -N ${job_cmds} -o ${o_log}_PALM_surfL.neg.log -e ${o_log}_PALM_surfL.neg.err -J ic${jj}_PALM.neg.L -K ${PALM_CMD_surf_L_neg}" >> ${LOGDIR}/dr.PALM
+      fi
+      dr_palm_cii=""; dr_palm_cii=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm; ls cort.R.neg/*.gii) )
+      if [[ ${#dr_palm_cii[@]} -eq 0 ]]; then
+        bsub -N ${job_cmds} -o ${o_log}_PALM_surfR.neg.log -e ${o_log}_PALM_surfR.neg.err -J ic${jj}_PALM.neg.R -K ${PALM_CMD_surf_R_neg} &
+        echo "bsub -N ${job_cmds} -o ${o_log}_PALM_surfR.neg.log -e ${o_log}_PALM_surfR.neg.err -J ic${jj}_PALM.neg.R -K ${PALM_CMD_surf_R_neg}" >> ${LOGDIR}/dr.PALM
+      fi
+    fi
   fi
   j=$(echo "${j} 1 + p" | dc -)
 done
@@ -1058,12 +1058,12 @@ while [ ${j} -lt ${Nics} ] ; do
   cort_R=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R; ls $(pwd)/*tfce*.gii | sort) )
   subcort=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/vol; ls $(pwd)/*tfce*.nii | sort) )
 
-  # # Add files to the array in the case of 1-sample t-test
-  # if [[ ${one_sample} = "true" ]]; then
-  #   cort_L+=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L.neg; ls $(pwd)/*tfce*.gii | sort) )
-  #   cort_R+=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R.neg; ls $(pwd)/*tfce*.gii | sort) )
-  #   subcort+=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/vol.neg; ls $(pwd)/*tfce*.nii | sort) )
-  # fi
+  # Add files to the array in the case of 1-sample t-test
+  if [[ ${one_sample} = "true" ]]; then
+    cort_L+=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.L.neg; ls $(pwd)/*tfce*.gii | sort) )
+    cort_R+=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/cort.R.neg; ls $(pwd)/*tfce*.gii | sort) )
+    subcort+=( $(cd ${OUTPUT}/dr_stage3_ic${jj}.palm/vol.neg; ls $(pwd)/*tfce*.nii | sort) )
+  fi
 
   # Write commands to internally compress gifti files if Octave was used
   if hash octave 2>/dev/null; then
